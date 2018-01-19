c82244b8af89bee16ef6efe1f247b0f9a64f8956
Drew Tucker <dtuck43@gmail.com>
2018-01-19 12:22:35 -0800

    Add back end logic to identify 0 and 1 in the inputArray and replace with beep or boop respectively.

--- js/scripts.js
+++ js/scripts.js
@@ -9,27 +9,38 @@ $(document).ready(function(){
 
 //***Back End Logic***//
 var inputArray = [];
-for (var index = 0; index < userInput; index += 1){
-  var push = inputArray.push(index);
+for (var i = 0; i <= userInput; i += 1){
+  inputArray.push(i);
+  inputArray.forEach(function(i){
+    if(inputArray[i] === 0){
+      inputArray[i] = "Beep!";
+    }
+    else if (inputArray[i] === 1){
+      inputArray[i] = "Boop!";
+    }
+    $("#result").text(inputArray);
+  });
 }
-console.log(inputArray);
-
-//Logic for if no number is inputted, or letters are inputted instead of numbers//
-if (isNaN(userInput)) {
-  alert("Please enter a number!");
-}
+// }
+// $("#result").text(inputArray);
+// console.log(inputArray);
+//
+// //Logic for if no number is inputted, or letters are inputted instead of numbers//
+// if (isNaN(userInput)) {
+//   alert("Please enter a number!");
+// }
 //Logic for numbers containing '1'//
-else if (userInput === 1){
-  $("#result").text("Boop!");
-}
+// else if (userInput === 1){
+//   $("#result").text("Boop!");
+// }
 //Logic for numbers containing '0'//
-else if (userInput === 0){
-  $("#result").text("Beep!");
-}
+// else if (userInput === 0){
+//   $("#result").text("Beep!");
+// }
 //Logic for numbers divisible by 3//
-else if ((userInput % 3) === 0) {
-  $("#result").text("I'm sorry Dave, i'm afraid I can't do that.");
-}
+// else if ((userInput % 3) === 0) {
+//   $("#result").text("I'm sorry Dave, i'm afraid I can't do that.");
+// }
 
 
 
