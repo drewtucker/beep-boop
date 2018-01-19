86835e4c47125203b5705d705079831eaf55f46f
Drew Tucker <dtuck43@gmail.com>
2018-01-19 11:18:12 -0800

    Add back end logic to take userInput data and push it into a blank array for furth manipulation.

--- js/scripts.js
+++ js/scripts.js
@@ -8,14 +8,15 @@ $(document).ready(function(){
 
 
 //***Back End Logic***//
+var inputArray = [];
+for (var index = 0; index < userInput; index += 1){
+  var push = inputArray.push(index);
+}
+console.log(inputArray);
 
 //Logic for if no number is inputted, or letters are inputted instead of numbers//
 if (isNaN(userInput)) {
   alert("Please enter a number!");
-}
-//Logic for numbers divisible by 3//
-else if ((userInput % 3) === 0) {
-  $("#result").text("I'm sorry Dave, i'm afraid I can't do that.");
 }
 //Logic for numbers containing '1'//
 else if (userInput === 1){
@@ -24,6 +25,10 @@ else if (userInput === 1){
 //Logic for numbers containing '0'//
 else if (userInput === 0){
   $("#result").text("Beep!");
+}
+//Logic for numbers divisible by 3//
+else if ((userInput % 3) === 0) {
+  $("#result").text("I'm sorry Dave, i'm afraid I can't do that.");
 }
 
 
