b7765c4514977b1740c1d585c46d92d579937291
Drew Tucker <dtuck43@gmail.com>
2018-01-19 10:30:25 -0800

    Add back end logic for if user inputs '0' as a number.

--- js/scripts.js
+++ js/scripts.js
@@ -10,14 +10,14 @@ $(document).ready(function(){
 //***Back End Logic***//
 
 //Logic for if no number is inputted, or letters are inputted instead of numbers//
-
 if (isNaN(userInput)) {
   alert("Please enter a number!");
+}
+//Logic for numbers containing '0'//
+else if (userInput === 0){
+  $("#result").text("Beep!");
 
 };
-//Logic for numbers containing '0'//
-
-if (userInput === ""){};
 
 
 
